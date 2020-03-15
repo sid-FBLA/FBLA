e8eb8e18e88d907e1f67817d0b4e4a55c5849c92
sid-FBLA <sid20102004@gmail.com>
2020-03-15 19:11:43 -0400

    Styling

--- JS/load.js
+++ JS/load.js
@@ -1,8 +1,17 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log("DOM content fully loaded and parsed");
+
+  const dropdown = document.querySelector('.main-nav');
+  const body = document.querySelector('body');
 
 
+  function create_element(type, parent, classtype) {
+    var identify = document.createElement(type);
+    identify.classList.add(classtype);
+    parent.appendChild(identify);
+    console.log(identify);
+    return identify;
+  };
 
-function addClass(e) {
-  $.get('HTML/bookings.html', null, function(text){
-    alert($(text).find('#arrival-location'));
-  });
-}
+  navDiv = document.createElement('DIV', )
+});
